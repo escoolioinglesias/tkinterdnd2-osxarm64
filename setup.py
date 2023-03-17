15d3ecf186eca080712e3863ff3ca0e2f9da2449
blacklein <69177971+blacklein@users.noreply.github.com>
2023-03-17 17:30:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tkinterdnd2",
-    version="0.3.0",
-    author="petasis\\pmgagne\\eliav2",
+    name="tkinterdnd2-osxarm",
+    version="1.0.0",
+    author="petasis\\pmgagne\\eliav2\\blacklein",
     description="TkinterDnD2 is a python wrapper for George Petasis'' tkDnD Tk extension version 2",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Eliav2/tkinterdnd2",
+    url="https://github.com/blacklein/tkinterdnd2-osxarm64",
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data={
